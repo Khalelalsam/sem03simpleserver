2b90045037b6f1947434fbd77cdfe0a123e58ad7
Khalelalsam <92095776+Khalelalsam@users.noreply.github.com>
2023-04-24 09:58:07 +0200

    Update tcp_server.go

--- tcp_server.go
+++ tcp_server.go
@@ -45,31 +45,12 @@ func main() {
 						}
 						return // fra for løkke
 					}
-
-					 dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-                                        log.Println("Dekrypter melding: ", string(dekryptertMelding))
-					switch msg := string(dekryptertMelding); msg {
-					case "ping":
+					switch msg := string(buf[:n]); msg {
+  				        case "ping":
 						_, err = c.Write([]byte("pong"))
-                                          case "Kjevik;SN39040;18.03.2022 01:50;6":
-                                  parts := strings.Split(msg, ";")
-                                    if len(parts) < 4 {
-                                  log.Println("Invalid input message")
-                                       return     }
-                     t, err := strconv.ParseFloat(parts[len(parts)-1], 64)
-                     if err != nil {
-                     log.Println(err) }
-
-  f := conv.CelsiusToFahrenheit(t)
-
-
-                           response:= fmt.Sprintf("Kjevik;SN39040;18.03.2022 01:50;%0.2f", f)
-                           _, err = c.Write([]byte(response))
-
 					default:
-log.Println("Dekrypter melding:---- ", string(dekryptertMelding))
-						_, err = c.Write(buf[:n])					}
-
+						_, err = c.Write(buf[:n])
+					}
 					if err != nil {
 						if err != io.EOF {
 							log.Println(err)
