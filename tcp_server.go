787a34b1cd4316c97238e186128764aaff4b720f
Khalelalsam <92095776+Khalelalsam@users.noreply.github.com>
2023-04-26 14:08:43 +0200

    Update tcp_server.go

--- tcp_server.go
+++ tcp_server.go
@@ -1,59 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"funtemps/conv"
-	"io"
-	"log"
-	"net"
-	"strconv"
-	"strings"
-	"sync"
-
-     
-
-	"github.com/Khalelalsam/is105sem03/mycrypt"
+        "fmt"
+        "funtemps/conv"
+        "io"
+        "log"
+        "net"
+        "strconv"
+        "strings"
+        "sync"
+        "github.com/Khalelalsam/is105sem03/mycrypt"
 )
 
 func main() {
 
-	var wg sync.WaitGroup
+        var wg sync.WaitGroup
 
-	server, err := net.Listen("tcp", "172.17.0.3:8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("bundet til %s", server.Addr().String())
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			log.Println("før server.Accept() kallet")
-			conn, err := server.Accept()
-			if err != nil {
-				return
-			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-					
-					
+        server, err := net.Listen("tcp", "172.17.0.3:8080")
+        if err != nil {
+                log.Fatal(err)
+        }
+        log.Printf("bundet til %s", server.Addr().String())
+        wg.Add(1)
+        go func() {
+                defer wg.Done()
+                for {
+                        log.Println("før server.Accept() kallet")
+                        conn, err := server.Accept()
+                        if err != nil {
+                                return
+                        }
+                        go func(c net.Conn) {
+                                defer c.Close()
+                                for {
+                                        buf := make([]byte, 1024)
+                                        n, err := c.Read(buf)
+                                        if err != nil {
+                                                if err != io.EOF {
+                                                        log.Println(err)
+                                                }
+                                                return // fra for løkke
+                                        }
+
+
 dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 log.Println("Dekrypter melding: ", string(dekryptertMelding))
-					
+
 switch msg := string(dekryptertMelding); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
-						
-						 case "Kjevik;SN39040;18.03.2022 01:50;6":
+                                        case "ping":
+                                                _, err = c.Write([]byte("pong"))
+
+                                                 case "Kjevik;SN39040;18.03.2022 01:50;6":
                                   parts := strings.Split(msg, ";")
                                     if len(parts) < 4 {
                                   log.Println("Invalid input message")
@@ -61,25 +58,27 @@ switch msg := string(dekryptertMelding); msg {
                      t, err := strconv.ParseFloat(parts[len(parts)-1], 64)
                      if err != nil {
                      log.Println(err) }
- 
+
                      f := conv.CelsiusToFahrenheit(t)
 
 
                            response:= fmt.Sprintf("Kjevik;SN39040;18.03.2022 01:50;%0.2f", f)
-                           _, err = c.Write([]byte(response))
+    encryptedResponse := mycrypt.Krypter([]rune(response), mycrypt.ALF_SEM03, 4)
+    _, err = c.Write([]byte(string(encryptedResponse)))
+//                           _, err = c.Write([]byte(response))
 
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+                                        default:
+                                                _, err = c.Write(buf[:n])
+                                        }
+                                        if err != nil {
+                                                if err != io.EOF {
+                                                        log.Println(err)
+                                                }
+                                                return // fra for løkke
+                                        }
+                                }
+                        }(conn)
+                }
+        }()
+        wg.Wait()
 }
